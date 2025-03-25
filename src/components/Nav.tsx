c22dd379f28a48dcf322ec7eb5242bd2b50e6601
Ruben Gullborg Frandsen <rufr@netcompany.com>
2025-03-25 15:09:17 +0100

    Implement front page

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import { Image } from "astro:assets";
-import Avatar from "../Assets/Avatar.jpg"
+import Avatar from "../Assets/Avatar.jpg";
 import { motion } from "framer-motion";
 import { useState } from "react";
 import Styles from "../styles/global.css";
@@ -8,17 +8,60 @@ import Styles from "../styles/global.css";
 export default function Nav() {
   const [toggled, setToggled] = useState(false);
   return (
-    <nav className="relative mx-8 mb-24">
-      <svg width="250" height={4} viewBox="0 0 250 4" fill="none">
-        <path d="M2 2L428 2" strokeWidth={2} stroke="#282828" strokeLinecap="round" />
+    <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
+      <svg
+        className="absolute bottom-0 -translate-x-1/2 left-1/2"
+        width="350"
+        height={4}
+        viewBox="0 0 250 4"
+        fill="none"
+      >
+        <path
+          d="M2 2L428 2"
+          strokeWidth={2}
+          stroke="#282828"
+          strokeLinecap="round"
+        />
       </svg>
-	  <div>
-		<img src={Avatar.src} alt="Profile picture of me" className="rounded-4xl max-h-14" />
-	  </div>
+      <div>
+        <img
+          src={Avatar.src}
+          alt="Profile picture of me"
+          className="rounded-4xl max-h-14"
+        />
+      </div>
 
-	  <h1 className="text-lg font-bold">
-		<a href="/">Cached thoughts and ideas.</a>
-	  </h1>
+      <h1 className="text-lg font-bold">
+        <a href="/">L1 Cache</a>
+      </h1>
+
+      <div className="flex gap-12 max-xl:hidden">
+        <a href="/">Home </a>
+        <a href="/">L1 Letter</a>
+        <a href="/">L2 Image</a>
+        <a href="/">L3 Video</a>
+        <a href="/">Contact </a>
+      </div>
+
+      <div
+        onClick={() => {
+          setToggled((prevToggle) => !prevToggle);
+          console.log(!toggled); // Tjekker den nye værdi
+        }}
+        className="space-y-1 cursor-pointer xl:hidden"
+      >
+        <span className="block h-0.5 w-8 bg-black"></span>
+        <span className="block h-0.5 w-6 bg-black"></span>
+        <span className="block h-0.5 w-4 bg-black"></span>
+      </div>
+
+      {/* Toggle this later for mobile menu  <div className="flex gap-12">
+          <a href="/">Home </a>
+          <a href="/">L1 Letter</a>
+          <a href="/">L2 Image</a>
+          <a href="/">L3 Video</a>
+          <a href="/">Contact </a>
+        </div> */}
     </nav>
   );
 }
